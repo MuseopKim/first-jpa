dc7951dcc4df21e7ebcd537c78a6822462cf581a
ragdoll <museopkim0214@gmail.com>
2020-07-29 03:29:19 +0900

    Feat: Member 관련 로직 구현
    - MemberRepository
      - save() : Member 저장
      - findOne() : id를 파라미터로 받아서 단일 member를 조회
      - findAll() : 모든 member를 리스트로 조회
      - findByName() : name을 파라미터로 받아서 단일 member 조회
    
    - MemberService
      - join() : 가입 로직 구현 - 이미 회원이 존재하는 검증 과정 수행
      - validateDuplicateMember() : 이미 존재하는 회원인지를 검증
      - findMembers() : 모든 회원 정보를 memberRepository를 통해 리턴
      - findOne() : id 값으로 회원을 memberRepository를 통해 조회

---
+++ first-jpa/src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public Long join(Member member) throws IllegalAccessException {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) throws IllegalAccessException {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalAccessException("이미 존재하는 회원 입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
---
+++ first-jpa/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,7 @@
+package com.jpabook.jpashop.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+}
