c88ba4251740ed1457f73af8943ebf23a6b20bd6
ragdoll <museopkim0214@gmail.com>
2020-08-07 22:01:17 +0900

    Feat: Member 관련 로직 테스트 케이스 추가
    - 회원가입 테스트
    - 중복 회원 예외처리 테스트

--- first-jpa/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
+++ first-jpa/src/test/java/com/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,7 +1,40 @@
 package com.jpabook.jpashop.service;
 
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
 
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setName("kim");
+
+        Long saveId = memberService.join(member);
+
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("kim1");
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        memberService.join(member1);
+        Throwable e = assertThrows(IllegalAccessException.class, () -> memberService.join(member2));
+        assertEquals("이미 존재하는 회원 입니다.", e.getMessage());
+    }
 }
