29eb6f23922674143af7b3b58ba04b3e4e7b6038
ragdoll <museopkim0214@gmail.com>
2020-09-19 16:17:13 +0900

    Feat : OrderService 서비스 로직 추가
    OrderItem
    - @NoArgsConstructor : 생성자 생성 방식을 사용하지 못하도록 생성자의 접근 제어자를 PROTECTED로 설정
    
    OrderService
    - order() : 주문이 들어오면 새로운 주문 정보를 저장한다.
       - Member, Delivery, OrderItem을 도메인 모델에서 조회 및 값을 세팅한 뒤 order를 save (도메인 모델 패턴 적용)
    - cancelOrder() : 주문이 취소되면 Order 엔티티에서 cancel() 호출하여 상태를 복구

---
+++ first-jpa/src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,56 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Delivery;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.repository.ItemRepository;
+import com.jpabook.jpashop.repository.MemberRepository;
+import com.jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // CASCADE ALL : 해당 엔티티를 하나의 엔티티에 속한 리스트에서만 관리를 할 경우 CASCADE를 사용해도 된다.
+        // 도메인 모델 패턴을 사용 했기에 서비스 계층은 도메인의 로직 처리를 위임하는 역할만 하게 된다.
+        // cf. 트랜잭션 스크립트 패턴
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+//    public List<Order> findOne(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
