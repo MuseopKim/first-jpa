c18d3b210beddc9dbfd1cada75f37b90c6010f7f
ragdoll <museopkim0214@gmail.com>
2020-09-19 17:57:08 +0900

    Feat : OrderServiceTest (OrderService 테스트) 추가
    OrderServiceTest
    - 상품주문()
    - 상품주문_재고수량초과() : 예외 테스트
    - 주문취소()

--- first-jpa/src/main/java/com/jpabook/jpashop/domain/Address.java
+++ first-jpa/src/main/java/com/jpabook/jpashop/domain/Address.java
@@ -1,12 +1,16 @@
 package com.jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private String city;
